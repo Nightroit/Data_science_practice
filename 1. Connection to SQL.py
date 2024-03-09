b18e05758c2629e3c5895fabd8876ca4e9159cf5
Shivam <s.sharma26626@gmail.com>
2024-03-09 06:00:32 +0530

    Day 1

---
+++ 1. Connection to SQL.py
@@ -0,0 +1,36 @@
+import mysql.connector
+import pandas as pd
+
+# Connect to MySQL
+connection = mysql.connector.connect(
+    user='Shivam',
+    password='slowly',
+    host='localhost',
+    database='temp'
+)
+
+cursor = connection.cursor()
+
+query_select = "SELECT * FROM Employee;"
+cursor.execute(query_select)
+
+result_set = cursor.fetchall()
+
+
+column_names = []
+
+for x in cursor.description:
+    column_names.append(x[0])
+dictionary = {}
+
+for column_idx, column_name in enumerate(column_names):
+    dictionary[column_name] = []
+    
+    for row in result_set:
+        dictionary[column_name].append(row[column_idx])
+
+print(pd.DataFrame(dictionary))
+
+connection.close()
+
+
