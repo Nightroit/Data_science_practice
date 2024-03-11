66c4b3e136fe4cc1e4b9f5c4792c49da3419ce1f
Shivam <s.sharma26626@gmail.com>
2024-03-11 07:19:58 +0530

    Day 3: Task 4

--- 3. Data Cleaning.py
+++ 3. Data Cleaning.py
@@ -11,23 +11,26 @@ original_data = {
     'diagnosis': ['Fever', 'Common Cold', 'Injury', 'N/A', 'Flu']
 }
 
-# Task 1 
+# Task 1: Cleaning the data 
 data = pd.DataFrame(original_data)
 data.replace('N/A', None, inplace=True)
 data.dropna(inplace=True)
 
 
-# Task 2: seperate the systolic and diastolic value from the blood_pressure column.
+# Task 2: Seperate the systolic and diastolic value from the blood_pressure column.
 blood_pressure_systolic = []
 blood_pressure_diastolic = []
 
 for values in data['blood_pressure']: 
-    blood_pressure_systolic.append(values.split('/')[0])
-    blood_pressure_diastolic.append(values.split('/')[1])
+    systolic, diastolic = map(int, values.split('/'))
+    blood_pressure_systolic.append(systolic)
+    blood_pressure_diastolic.append(diastolic)
 
+# Task 3: Extract and separate the systolic and diastolic values from the 'blood_pressure' column.    
 del original_data['blood_pressure']
-original_data['blood_pressure_systolic'] = blood_pressure_systolic
-original_data['blood_pressure_diastolic']  = blood_pressure_diastolic
+original_data['blood_pressure_systolic'] = pd.Series(blood_pressure_systolic)
+original_data['blood_pressure_diastolic']  = pd.Series(blood_pressure_diastolic)
 
-print(original_data)
-    
+# Task 4: Calculate the average temperature and heart rate for the patients.
+print(round(data['temperature'].mean(), 2))
+print(round(data['heart_rate'].mean(), 2))
