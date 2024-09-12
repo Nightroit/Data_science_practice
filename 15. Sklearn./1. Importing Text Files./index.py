845cbe0d22b196345a4803775b36ae11910d298b
root <root@kali>
2024-09-12 02:23:03 -0400

    Day 1: Basic text classification using naive_bayes

--- 15. Sklearn./1. Importing Text Files./index.py
+++ 15. Sklearn./1. Importing Text Files./index.py
@@ -1,12 +1,20 @@
 from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
-dataset = load_files("./data", categories = ["class1", "class2"])
+datasets = load_files("./data", categories = ["class1", "class2"])
 
-data = dataset.data
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(datasets.data)
+y = datasets.target
 
-for elements in data:
-    print(elements)
 
-for elements in dataset.target:
-    print(elements)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
+
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+print(accuracy_score(y_pred, y_test))
